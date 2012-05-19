8f1d5d773f441fac7c02a53c9e569d674b003476
G Krupa <gkrupa@cars.com>
2012-05-18 21:03:02 -0700

    Mess with time.time() to get desired effect Don't bother messing with datetime.today() or similar

--- src/test/tests.py
+++ src/test/tests.py
@@ -4,26 +4,38 @@ import mock
 from datetime import date
 import time
 
-
 # initial implementation taken from http://stackoverflow.com/questions/4481954/python-trying-to-mock-datetime-date-today-but-not-working
 
 
-class FakeDate(date):
+class PyTimeCop(object):
     "A manipulable date replacement"
-    def __new__(cls, *args, **kwargs):
-        return date.__new__(date, *args, **kwargs)
 
+    # have we already patched the system?
+    patched_ = False
+
+    def __init__(self):
+        self.ensure_patched_()
+
+    @classmethod
+    def ensure_patched_(self):
+        if not PyTimeCop.patched_:
+            self.patch_system_()
+
+    @classmethod
+    def patch_system_(cls):
+        cls.patched_ = True
+        def my_time():
+            return cls.time_
+        time.time = my_time
+
+    @classmethod
+    def be_time(cls, secs):
+        cls.ensure_patched_()
+        cls.time_ = secs
 
 class TestTimeCop(unittest.TestCase):
 
-    @mock.patch('datetime.date', FakeDate)
     def test_fake_time(self):
-        from datetime import date
-
-        @classmethod
-        def date_(cls):
-            return date(2010, 1, 1)
-
-        FakeDate.today = date_
+        PyTimeCop.be_time(1330399987.0)
         print date.today()
 
