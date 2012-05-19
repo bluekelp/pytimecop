d01b78ad276b61e41e69ee0dc4140c5c0a4c26dd
G Krupa <gkrupa@cars.com>
2012-05-18 21:27:44 -0700

    Better impl. Move to own module.

--- src/test/tests.py
+++ src/test/tests.py
@@ -1,41 +1,25 @@
+
 import unittest
-import mock
 
 from datetime import date
+from datetime import timedelta
 import time
 
-# initial implementation taken from http://stackoverflow.com/questions/4481954/python-trying-to-mock-datetime-date-today-but-not-working
-
-
-class PyTimeCop(object):
-    "A manipulable date replacement"
-
-    # have we already patched the system?
-    patched_ = False
-
-    def __init__(self):
-        self.ensure_patched_()
-
-    @classmethod
-    def ensure_patched_(self):
-        if not PyTimeCop.patched_:
-            self.patch_system_()
-
-    @classmethod
-    def patch_system_(cls):
-        cls.patched_ = True
-        def my_time():
-            return cls.time_
-        time.time = my_time
-
-    @classmethod
-    def be_time(cls, secs):
-        cls.ensure_patched_()
-        cls.time_ = secs
+from PyTimeCop import PyTimeCop
 
 class TestTimeCop(unittest.TestCase):
 
-    def test_fake_time(self):
-        PyTimeCop.be_time(1330399987.0)
-        print date.today()
+    def test_yesterday(self):
+        secs = time.time()
+        secs -= 60*60*24  # approximate yesterday
+
+        # do it "right"
+        yesterday = date.today() - timedelta(days=1)
+
+        with PyTimeCop(secs):
+            self.assertEqual(yesterday, date.today())
+
+    def test_epoch(self):
+        with PyTimeCop(0):
+            self.assertEqual(date(1969, 12, 31), date.today())
 
